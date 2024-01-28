7431377243d1bf5a49b674dc6f2b67fc309a09fe
fishfoldyy <145004951+fishfoldyy@users.noreply.github.com>
2024-01-28 09:22:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,15 @@ function revealCell(cellsArray, row, col) {
     cell.classList.add('clicked');
     const count = countAdjacentMines(cellsArray, row, col);
     if (count === 0) {
-      // Loop through adjacent cells and reveal each if not already clicked
       for (let i = row - 1; i <= row + 1; i++) {
         for (let j = col - 1; j <= col + 1; j++) {
           if (i >= 0 && i < 10 && j >= 0 && j < 10 && !(i === row && j === col)) {
-            revealCell(cellsArray, i, j); // Recursively reveal adjacent cell
+            revealCell(cellsArray, i, j);
           }
         }
       }
     } else if (count > 0) {
-      cell.textContent = count; // Set number if cell has adjacent mines
+      cell.textContent = count;
     }
   }
 }
